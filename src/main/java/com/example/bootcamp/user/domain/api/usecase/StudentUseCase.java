822ba502ccf9d7d42d5651155d75a90b42518394
Mateo Velásquez Morales <matius29002@gmail.com>
2025-03-22 15:14:36 -0500

    fix: validation method

--- src/main/java/com/example/bootcamp/user/domain/api/usecase/StudentUseCase.java
+++ src/main/java/com/example/bootcamp/user/domain/api/usecase/StudentUseCase.java
@@ -10,6 +10,7 @@ import com.example.bootcamp.user.domain.spi.IStudentPersistencePort;
 import static com.example.bootcamp.user.domain.util.StudentMessage.*;
 import static com.example.bootcamp.user.domain.util.Validations.*;
 import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
 
 public class StudentUseCase implements IStudentServicePort {
 
@@ -26,29 +27,19 @@ public class StudentUseCase implements IStudentServicePort {
     @Override
     public void save(Student student, StudentInstitution studentInstitution) {
 
-        if(!isValidEmail(student.getEmail())){
-            throw new InvalidEmailException(EMAIL_INVALID_MESSAGE);
-        }
+        if(!isValidEmail(student.getEmail())) throw new InvalidEmailException(EMAIL_INVALID_MESSAGE);
 
-        if(!isValidCellphone(student.getCellphone())){
-            throw new InvalidCellphoneException(CELLPHONE_INVALID_MESSAGE);
-        }
+        if(!isValidCellphone(student.getCellphone())) throw new InvalidCellphoneException(CELLPHONE_INVALID_MESSAGE);
 
-        if(!isValidIdentification(student.getIdentification())){
-            throw new InvalidIdentificationException(IDENTIFICATION_INVALID_MESSAGE);
-        }
+        if(!isValidIdentification(student.getIdentification())) throw new InvalidIdentificationException(IDENTIFICATION_INVALID_MESSAGE);
 
-        if(!isAdult(student.getBirthDate())){
-            throw new InvalidAgeException(STUDENT_IS_NOT_AN_ADULT);
-        }
+        if(!isAdult(student.getBirthDate())) throw new InvalidAgeException(STUDENT_IS_NOT_AN_ADULT);
 
-        if(!isNull(studentPersistencePort.findByEmailOrIdentification(student.getEmail(), student.getIdentification()))){
+        if(nonNull(studentPersistencePort.findByEmailOrIdentification(student.getEmail(), student.getIdentification())))
             throw new StudentExistsException(STUDENT_EXIST);
-        }
 
-        if(isNull(institutionPersistencePort.findById(studentInstitution.getInstitution().getId()))){
+        if(isNull(institutionPersistencePort.findById(studentInstitution.getInstitution().getId())))
             throw new InstitutionNotFoundException(INSTITUTION_NOT_FOUND);
-        }
 
         studentPersistencePort.save(student, studentInstitution);
     }
